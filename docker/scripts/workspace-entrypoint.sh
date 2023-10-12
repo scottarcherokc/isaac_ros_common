06eee78ece1d5c0daef866138c1a59b3f8dac713
Jacob Dahl <dahl.jakejacob@gmail.com>
2023-10-12 15:20:22 -0800

    updated workspace-entry.sh

--- docker/scripts/workspace-entrypoint.sh
+++ docker/scripts/workspace-entrypoint.sh
@@ -12,11 +12,8 @@
 echo "source /opt/ros/${ROS_DISTRO}/setup.bash" >> ~/.bashrc
 source /opt/ros/${ROS_DISTRO}/setup.bash
 
-# sudo apt-get update
-# rosdep update
-
 # Restart udev daemon
 echo "restarting udev.. why tho"
 sudo service udev restart
 
-$@
+echo "Docker container is up!"
